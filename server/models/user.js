fb8616cd450611c3d0306bd01265a978de6df86e
riankarya <riankaryamarpaung@gmail.com>
2020-12-11 11:45:27 +0700

    minor fix

--- server/models/user.js
+++ server/models/user.js
@@ -20,7 +20,10 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     email: {
       type: DataTypes.STRING,
-      unique: true,
+      unique: {
+        args: true,
+        msg: 'The email is already taken!'
+      },
       validate: {
         customValidator(value) {
           let reg = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
